38ea88de2d3fed4fe850dbcc1c1607a4f9cc0a7a
Almaz <diamond-4d@mail.ru>
2022-02-19 13:57:16 +0300

    add

--- src/redux/catalog/actions.ts
+++ src/redux/catalog/actions.ts
@@ -2,7 +2,13 @@ import { IProductItem } from "../../types";
 
 export const SET_PRODUCTS = "SET_PRODUCTS";
 
+export const LOAD_PRODUCTS_START = "LOAD_PRODUCTS_START";
+export const LOAD_PRODUCTS_END = "LOAD_PRODUCTS_END";
+
 export const setProducts = (products: IProductItem[]) => ({
   type: SET_PRODUCTS,
   products,
 });
+
+export const loadProductsStart = () => ({ type: LOAD_PRODUCTS_START });
+export const loadProductsEnd = () => ({ type: LOAD_PRODUCTS_END });
