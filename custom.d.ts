c0042c1ee9bad49a10222e876f502124f19be3ad
Almaz <diamond-4d@mail.ru>
2022-02-20 18:35:21 +0300

    lint custom.d.ts

--- custom.d.ts
+++ custom.d.ts
@@ -1,14 +1,14 @@
-declare module "*.png" {
+declare module '*.png' {
   const content: any;
   export default content;
 }
 
-declare module "*.svg" {
-  import React = require("react");
+declare module '*.svg' {
+  import React = require('react');
   export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>;
   const src: string;
   export default src;
 }
 
-declare module "react-router-dom";
-declare module "react-modal";
+declare module 'react-router-dom';
+declare module 'react-modal';
